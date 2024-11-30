00faee6756bc377f183e3026021e69b5b6568500
Christophar Antony <christopharantony@gmail.com>
2024-12-01 01:43:11 +0530

    🔥 feat: Add motion animations for modal transitions (appearance and disappearance)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,21 @@
-import { createPortal } from 'react-dom';
+import { motion } from "motion/react";
+import { createPortal } from "react-dom";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        open
+        className="modal"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 80, opacity: 0 }}
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
